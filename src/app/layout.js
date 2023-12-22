75e8127ada9adcbe6cf9c568c69e887ea70d1326
jnoodle <fullstack@126.com>
2023-12-22 11:13:02 +0800

    feat: suspense

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Container from "@/app/components/container";
@@ -8,11 +9,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout(props) {
   return (
-    <html lang="en">
-      <title>Cfxs Marketspace</title>
-      <body className={inter.className}>
-        <Container {...props} />
-      </body>
-    </html>
+    <Suspense>
+      <html lang="en">
+        <title>Cfxs Marketspace</title>
+        <body className={inter.className}>
+          <Container {...props} />
+        </body>
+      </html>
+    </Suspense>
   );
 }
