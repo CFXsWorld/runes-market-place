816c76f60850e489bf93144dc5dbbf669fe749c2
murrayee <murrayee@163.com>
2024-01-10 12:57:47 +0800

    fix: wrap

--- src/app/(pages)/market/_components/Filter/Refresh.jsx
+++ src/app/(pages)/market/_components/Filter/Refresh.jsx
@@ -6,7 +6,7 @@ import { cn } from '@/app/utils/classnames';
 const Refresh = ({ className }) => {
   return (
     <div className={cn('flex-center', className)}>
-      <span className='text-tc-secondary'>Result: 1538</span>
+      <span className='text-tc-secondary flex-shrink-0'>Result: 1538</span>
       <RefreshIcon className="mx-[16px] cursor-pointer hover:opacity-80 max-md:mr-0" />
     </div>
   );
