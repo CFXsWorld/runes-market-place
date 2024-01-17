31a0d20404169436987d4aa757b30389fea6e57f
murrayee <murrayee@163.com>
2024-01-17 23:30:19 +0800

    chore: fix layout

--- src/app/(pages)/my/_components/Filter/Refresh.jsx
+++ src/app/(pages)/my/_components/Filter/Refresh.jsx
@@ -2,12 +2,28 @@
 
 import { RefreshIcon } from '@/app/components/icons';
 import { cn } from '@/app/utils/classnames';
+import useCFXsContract from '@/app/hooks/useCFXsContract';
+import { useWalletStore } from '@/app/store/wallet';
+import { useEffect, useState } from 'react';
+import { getAddress, isAddress } from 'ethers';
 
 const Refresh = ({ className, reload, total }) => {
+  const [balance, setBalance] = useState(0);
+  const { contract } = useCFXsContract();
+  const account = useWalletStore((state) => state.account);
+
+  useEffect(() => {
+    if (isAddress(account)) {
+      contract.balanceOf(getAddress(account)).then((res) => {
+        setBalance(res.toString());
+      });
+    }
+  }, [account]);
   return (
     <div className={cn('flex-center', className)}>
-      <span className="text-tc-secondary flex-shrink-0">
-        Slots: {total || 0}
+      <span className="text-tc-secondary flex-shrink-0 flex-center gap-[12px]">
+        <span>Slots: {total || 0} </span>
+        <span>Balance: {balance || 0}</span>
       </span>
       <RefreshIcon
         className="mx-[16px] cursor-pointer hover:opacity-80 max-md:mr-0"
