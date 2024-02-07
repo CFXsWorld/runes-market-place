c0bd9ef11daf017110181afd1e1cffd78f62f8cd
murrayee <murrayee@163.com>
2024-02-07 17:27:19 +0800

    chore: reward

--- src/app/components/Reward/RewardModal.jsx
+++ src/app/components/Reward/RewardModal.jsx
@@ -2,11 +2,12 @@
 
 import { Modal } from 'flowbite-react';
 import useRewardStore from '@/app/store/reward';
-import { CloseIcon } from '@/app/components/icons';
+import { CloseIcon, LoadingIcon } from '@/app/components/icons';
 import { useWalletStore } from '@/app/store/wallet';
 import WithConnect from '@/app/components/Wallet/WithConnect';
 import { toast } from 'react-toastify';
 import { addressFormat } from '@/app/utils';
+import useReward from '@/app/components/Reward/useReward';
 const RewardConnect = ({
   isConnected,
   isActive,
@@ -14,6 +15,7 @@ const RewardConnect = ({
   isCorrectChain,
   switchChain,
   correctChainIdHex,
+  count,
 }) => {
   const onOpen = useWalletStore((state) => state.onOpen);
   const switchNetwork = async () => {
@@ -42,8 +44,9 @@ const RewardConnect = ({
       );
     }
     return (
-      <div className="ml-[5px] flex-center h-[44px] my-[32px] text-theme">
-        {addressFormat(account)}
+      <div className="ml-[5px] flex items-center justify-center flex-col h-[44px] my-[32px] text-theme">
+        <p>{addressFormat(account)}</p>
+        <p className="text-[12px]">剩余 {count} 次</p>
       </div>
     );
   }
@@ -64,6 +67,60 @@ const RewardModal = () => {
   const open = useRewardStore((state) => state.open);
   const onOpen = useRewardStore((state) => state.onOpen);
   const onOpenWallet = useWalletStore((state) => state.onOpen);
+  const { count, status, openLoading, amount, openReward, setStatus } =
+    useReward();
+
+  const renderRedBg = () => {
+    if (status === 'opened') {
+      return (
+        <>
+          <div
+            className="w-[270px] h-[356px] bg-conver bg-center cursor-pointer flex justify-center"
+            style={{ backgroundImage: `url(/opened.png)` }}
+          >
+            <div>
+              <p className="text-[#9F7D54] text-[66px] mt-[66px]">{amount}</p>
+              <p className="text-[#9F7D54] ">CFXs</p>
+            </div>
+          </div>
+
+          <div
+            className="w-full text-black bg-theme h-[44px] rounded-[22px] my-[32px] flex-center cursor-pointer"
+            onClick={() => {
+              if (Number(count) > 0) {
+                openReward();
+              } else {
+                setStatus('closed');
+              }
+            }}
+          >
+            {Number(count) > 0 ? '继续领取' : '开心收下'}
+          </div>
+        </>
+      );
+    }
+    if (status === 'closed') {
+      return (
+        <div
+          className="w-[270px] h-[356px] bg-conver bg-center cursor-pointer flex justify-center"
+          style={{ backgroundImage: `url(/claimed.png)` }}
+        >
+          <p className="text-[#9F7D54] text-[24px] mt-[60px]">您已经领取过了</p>
+        </div>
+      );
+    }
+    return (
+      <div
+        className="w-[270px] h-[356px] bg-conver bg-center cursor-pointer flex-center"
+        style={{ backgroundImage: `url(/open.png)` }}
+        onClick={() => {
+          openReward();
+        }}
+      >
+        {openLoading && <LoadingIcon />}
+      </div>
+    );
+  };
   return (
     <Modal
       show={open}
@@ -74,14 +131,11 @@ const RewardModal = () => {
     >
       <Modal.Body>
         <div className="">
-          <div
-            className="w-[270px] h-[356px] bg-conver bg-center"
-            style={{ backgroundImage: `url(/open.png)` }}
-          >
-            {/*12313*/}
-          </div>
+          {renderRedBg()}
 
-          <WithConnect>{(props) => <RewardConnect {...props} />}</WithConnect>
+          <WithConnect>
+            {(props) => <RewardConnect {...props} count={count} />}
+          </WithConnect>
 
           <div className="flex-center">
             <span
--- src/app/components/Reward/useReward.js
+++ src/app/components/Reward/useReward.js
@@ -1,14 +1,81 @@
+import RedEnv from './redenv.json';
+import useWallet from '@/app/hooks/useWallet';
+import { useWalletStore } from '@/app/store/wallet';
+import { Contract, formatUnits, getAddress } from 'ethers';
+import { toast } from 'react-toastify';
+import { useEffect, useState } from 'react';
+const rpc = 'https://conflux-espace.rpc.thirdweb.com';
+const address = '';
+
 const useReward = () => {
-  const connectWallet = () => {};
+  const [count, setCount] = useState(0);
+  const { browserProvider } = useWallet();
+  const [status, setStatus] = useState('opening');
+  const [amount, setAmount] = useState(0);
+  const account = useWalletStore((state) => state.account);
+  const [openLoading, setOpenLoading] = useState(false);
 
-  const switchNetwork = () => {};
+  const getRemainCount = async () => {
+    if (account) {
+      try {
+        const RedContract = new Contract(
+          RedEnv.address,
+          RedEnv.abi,
+          browserProvider
+        );
+        const signer = await browserProvider.getSigner();
+        const contractWithSigner = RedContract.connect(signer);
+        const data = await contractWithSigner.getYourRemainTimes(
+          getAddress(account)
+        );
+        setCount(data + '');
+        if (data === 0) {
+          setStatus('closed');
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
 
-  const openReward = () => {};
+  useEffect(() => {
+    if (account && browserProvider) {
+      getRemainCount();
+    }
+  }, [account, browserProvider]);
+  const openReward = async () => {
+    if (account) {
+      try {
+        setOpenLoading(true);
+        const RedContract = new Contract(
+          RedEnv.address,
+          RedEnv.abi,
+          browserProvider
+        );
+        const signer = await browserProvider.getSigner();
+        const contractWithSigner = RedContract.connect(signer);
+        const tx = await contractWithSigner.receiveRed();
+        const receipt = await tx.wait();
+        const amount = formatUnits(receipt, 'ethers');
+        setAmount(amount);
+        setStatus('opened');
+        setCount((prev) => Number(prev) - 1);
+      } catch (e) {
+        console.log(e);
+        toast.error('未开始！');
+      } finally {
+        setOpenLoading(false);
+      }
+    }
+  };
 
   return {
-    connectWallet,
-    switchNetwork,
     openReward,
+    count,
+    openLoading,
+    status,
+    amount,
+    setStatus,
   };
 };
 
