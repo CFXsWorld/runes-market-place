81d5e89788737c73b0c9aed966d370ea85b93745
murrayee <murrayee@163.com>
2024-02-11 22:04:04 +0800

    chore: merge gis

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -100,7 +100,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <title>CFXs World</title>
       <body className={inter.className}>
-        <Dapp>{children}</Dapp>
+        {/*<Dapp>{children}</Dapp>*/}
+      
       </body>
     </html>
   );
