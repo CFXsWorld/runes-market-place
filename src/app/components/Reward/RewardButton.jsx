c6ad1202786a46d3043dc7e9efc7b72999c4dbd9
murrayee <murrayee@163.com>
2024-02-11 11:44:23 +0800

    chore: merge gis

--- src/app/components/Reward/RewardButton.jsx
+++ src/app/components/Reward/RewardButton.jsx
@@ -4,15 +4,16 @@ import useRewardStore from '@/app/store/reward';
 
 const RewardButton = () => {
   const onOpen = useRewardStore((state) => state.onOpen);
-  return (
-    <div
-      className="w-[88px] h-[116px] fixed right-[20px] md:top-[90px] bg-cover cursor-pointer max-md:bottom-[120px] max-md:w-[60px] max-md:h-[80px]"
-      style={{ backgroundImage: `url(/open.png)` }}
-      onClick={() => {
-        onOpen(true);
-      }}
-    ></div>
-  );
+  return  null;
+  // return (
+  //   <div
+  //     className="w-[88px] h-[116px] fixed right-[20px] md:top-[90px] bg-cover cursor-pointer max-md:bottom-[120px] max-md:w-[60px] max-md:h-[80px]"
+  //     style={{ backgroundImage: `url(/open.png)` }}
+  //     onClick={() => {
+  //       onOpen(true);
+  //     }}
+  //   ></div>
+  // );
 };
 
 export default RewardButton;
