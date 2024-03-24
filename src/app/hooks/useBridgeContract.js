a9580097bc616cc4420d07b9676d94af7057172f
murrayee <murrayee@163.com>
2024-03-24 13:11:36 +0800

    chore: fix error

--- src/app/hooks/useBridgeContract.js
+++ src/app/hooks/useBridgeContract.js
@@ -1,13 +1,18 @@
 import { abi } from '../contracts/bridgeContractMainnet.json';
 import useEnv from '@/app/hooks/useEnv';
 import useWallet from '@/app/hooks/useWallet';
-import { BrowserProvider, Contract } from 'ethers';
+import { Contract } from 'ethers';
+import { useMemo } from 'react';
 
 const useBridgeContract = () => {
   const { bridgeContractAddress } = useEnv();
-  const wallet = useWallet();
-  const provider = new BrowserProvider(wallet.provider);
-  const contract = new Contract(bridgeContractAddress, abi, provider);
+  const { browserProvider } = useWallet();
+
+  const contract = useMemo(() => {
+    return browserProvider && bridgeContractAddress
+      ? new Contract(bridgeContractAddress, abi, bridgeContractAddress)
+      : null;
+  }, [browserProvider, bridgeContractAddress]);
 
   return { contract };
 };
